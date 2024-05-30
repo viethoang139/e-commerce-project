bdc94c434d07bcd3855c4295df80aa0d74849442
Le Viet Hoang <levieth710@gmail.com>
2024-05-30 15:11:01 +0700

    add unique email feature

--- be/e-commerce-be/src/main/java/com/lvh/repository/CustomerRepository.java
+++ be/e-commerce-be/src/main/java/com/lvh/repository/CustomerRepository.java
@@ -4,4 +4,8 @@ import com.lvh.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String theEmail);
+
 }
+
--- be/e-commerce-be/src/main/java/com/lvh/service/impl/CheckoutServiceImpl.java
+++ be/e-commerce-be/src/main/java/com/lvh/service/impl/CheckoutServiceImpl.java
@@ -33,6 +33,15 @@ public class CheckoutServiceImpl implements CheckoutService {
         order.setShippingAddress(purchase.getShippingAddress());
 
         Customer customer = purchase.getCustomer();
+
+        String theEmail = customer.getEmail();
+
+        Customer customerFromDB = customerRepository.findByEmail(theEmail);
+
+        if(customerFromDB != null){
+            customer = customerFromDB;
+        }
+
         customer.add(order);
 
         customerRepository.save(customer);
